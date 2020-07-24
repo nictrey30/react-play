261e8ec1f2c272cf99b5df3b58a3ad100b53f9f2
Traian <niculescu.traian.ct@gmail.com>
2020-07-24 16:46:36 +0300

    react-todos-1 finished

---
+++ react-todos-1/src/TodoItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function TodoItem(props) {
+  return (
+    <div className='todo-item'>
+      <input
+        type='checkbox'
+        checked={props.item.completed}
+        onChange={() => {
+          props.callback(props.item.id);
+        }}
+      />
+      <p>{props.item.text}</p>
+    </div>
+  );
+}
+
+export default TodoItem;
